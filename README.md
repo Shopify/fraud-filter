264b32bc6375a1454daf2e6746668478ccc84843
Sonja Farrell <sonja.farrell@shopify.com>
2019-06-16 22:29:56 -0400

    🎊 updating step2

---
+++ README.md
@@ -0,0 +1,9 @@
+# Making Polaris work for you exmaple app
+
+This is the Fraud Filter app built with Polaris as an example for the Unite workshop.
+
+## Getting started
+
+1. `git clone git@github.com:Shopify/fraud-filter.git`
+1. `npm install`
+1. `npm start`
--- src/routes/FilterList.js
+++ src/routes/FilterList.js
@@ -2,30 +2,40 @@ import React from 'react';
 import {graphql} from 'react-apollo';
 import gql from 'graphql-tag';
 
-import {Page, Card, TextContainer, Tabs} from '@shopify/polaris';
+import {Page, Card, TextContainer, Tabs, SkeletonBodyText, SkeletonDisplayText, EmptyState} from '@shopify/polaris';
+
+import emptyStateBackground from './../images/empty-state-bg.png';
 import {selected, tabs} from './../stubData';
 
 function FilterList({data: {loading, filters}}) {
   /* Comment or uncomment the next two lines to toggle the loading state */
-  loading = true;
-  filters = null;
+  // loading = true;
+  // filters = null;
 
   /* Comment or uncomment the next line to toggle the empty state */
-  // filters = [];
+  filters = [];
 
   const loadingStateContent = loading ? (
     <Card sectioned>
       <TextContainer>
-        {/* Add the skeleton component and any necessary props here  */}
+        <SkeletonDisplayText size="small" />
+        <SkeletonBodyText />
+        <SkeletonBodyText />
       </TextContainer>
     </Card>
   ) : null;
 
   const emptyStateContent =
   filters && filters.length === 0
-    ? {
-        /* Add the empty state component and any necessary props here */
-    }
+    ? (
+      <EmptyState
+        heading="Add a layer of security to your store"
+        action={{content: 'Add a filter', url: '/'}}
+        image={emptyStateBackground}
+      >
+        <p>Create custom filters to help you prevent fraud.</p>
+      </EmptyState>
+    )
     : null;
 
   const filtersIndex =
